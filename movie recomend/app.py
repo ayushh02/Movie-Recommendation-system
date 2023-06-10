da0f88b57abc7df1e2eb492763c11c1ab78e8636
Ayush Kumar <72346028+ayushh02@users.noreply.github.com>
2023-06-10 12:43:28 +0530

    Add files via upload

---
+++ movie recomend/app.py
@@ -0,0 +1,43 @@
+import pickle
+from flask import Flask, render_template, request
+import requestsx    
+
+app = Flask(__name__)
+
+def fetch_poster(movie_id):
+    url = "https://api.themoviedb.org/3/movie/{}?api_key=8265bd1679663a7ea12ac168da84d2e8&language=en-US".format(movie_id)
+    data = requests.get(url)
+    data = data.json()
+    poster_path = data['poster_path']
+    full_path = "https://image.tmdb.org/t/p/w500/" + poster_path
+    return full_path
+
+def recommend(movie):
+    movies = pickle.load(open('movie_list.pkl','rb'))
+    similarity = pickle.load(open('similarity.pkl','rb'))
+    index = movies[movies['title'] == movie].index[0]
+    distances = sorted(list(enumerate(similarity[index])), reverse=True, key=lambda x: x[1])
+    recommended_movie_names = []
+    recommended_movie_posters = []
+    for i in distances[1:6]:
+        # fetch the movie poster
+        movie_id = movies.iloc[i[0]].movie_id
+        recommended_movie_posters.append(fetch_poster(movie_id))
+        recommended_movie_names.append(movies.iloc[i[0]].title)
+
+    return recommended_movie_names,recommended_movie_posters
+
+    
+@app.route("/")
+def home():
+    movies = pickle.load(open('movie_list.pkl','rb'))
+    return render_template('home.html', movie_list=list(movies['title'].values))
+
+@app.route("/recommend", methods=["POST"])
+def get_recommendations():
+    movie = request.form["movie"]
+    recommended_movie_names,recommended_movie_posters = recommend(movie)
+    return render_template('recommendations.html', movie_names=recommended_movie_names, movie_posters=recommended_movie_posters)
+
+if __name__ == "__main__":
+    app.run(debug=True)
